6661febba669c95efb0ae31de1d0880e912da674
surafel <surafelaron@gmail.com>
2021-07-01 14:38:02 -0700

    server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req,res) =>{
+    res.send('<h1> hi there </h1>')
+})
+
+app.listen(PORT, ()=> console.log(`app listinig on ${PORT}`));
